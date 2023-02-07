b99a6f1bb365e61d80fae21a7211631d4bd1f0fb
minh94dn <grakk2405@gmail.com>
2023-02-07 17:02:45 +0700

    up

--- case_study/case-study/src/app/app-routing.module.ts
+++ case_study/case-study/src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
+import {FacilityListComponent} from "./facility/facility-list/facility-list.component";
+import {CustomerListComponent} from "./component/customer/customer-list/customer-list.component";
+import {ContractListComponent} from "./component/contract/contract-list/contract-list.component";
+import {EmployeeListComponent} from "./component/employee/employee-list/employee-list.component";
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'facility', component: FacilityListComponent},
+  {path: 'customer', component: CustomerListComponent},
+  {path: 'contract', component: ContractListComponent},
+  {path: 'employee', component: EmployeeListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- case_study/case-study/src/app/app.module.ts
+++ case_study/case-study/src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { BodyComponent } from './body/body.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './component/footer/footer.component';
+import {HeaderComponent} from './component/header/header.component';
+import {HomeComponent} from './component/home/home.component';
+import {BodyComponent} from './component/body/body.component';
+import { FacilityListComponent } from './facility/facility-list/facility-list.component';
+import { FacilityEditComponent } from './facility/facility-edit/facility-edit.component';
+import { FacilityAddComponent } from './facility/facility-add/facility-add.component';
+import { EmployeeListComponent } from './component/employee/employee-list/employee-list.component';
+import { CustomerListComponent } from './component/customer/customer-list/customer-list.component';
+import { ContractListComponent } from './component/contract/contract-list/contract-list.component';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +22,13 @@ import { BodyComponent } from './body/body.component';
     FooterComponent,
     HeaderComponent,
     HomeComponent,
-    BodyComponent
+    BodyComponent,
+    FacilityListComponent,
+    FacilityEditComponent,
+    FacilityAddComponent,
+    EmployeeListComponent,
+    CustomerListComponent,
+    ContractListComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,4 +37,5 @@ import { BodyComponent } from './body/body.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
